f6a9eebecde52236ed38e4e9b9508198290ac08b
Yaclaudes Minoso <50747740+yaclaudes1@users.noreply.github.com>
2024-07-08 21:56:58 -0400

    Add files via upload

---
+++ SetRocmPowerAlways.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MAX_POWER=80
+while :
+do
+        echo "To quit press Ctrl-C"
+        sleep 5
+        read -r -d '' CURRENT_MAX_POWER < <(rocm-smi -M | grep -Eo "[0-9]+\.[0]")
+        read -r -d '' MAX_POWER_DECIMAL < <(bc <<< "$MAX_POWER != $CURRENT_MAX_POWER")
+        if [  "$MAX_POWER_DECIMAL" -eq 1 ]
+        then
+            yes | rocm-smi --setpoweroverdrive "$MAX_POWER"
+        fi
+done
+exit 0
